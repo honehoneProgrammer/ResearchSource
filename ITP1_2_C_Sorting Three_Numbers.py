7fad821b97d68504cfea9f235fd917cd8df1610a
honehoneProgrammer <GithubIbuki.com>
2022-07-11 00:10:59 +0900

    rangeの中身が整数じゃなかった

--- ITP1_2_C_Sorting Three_Numbers.py
+++ ITP1_2_C_Sorting Three_Numbers.py
@@ -2,5 +2,7 @@ num_list = [int(x) for x in input().split()]
 
 num_list.sort()
 
-for x in num_list:
-    print(x, end=" ")
+for x in range(num_list):
+    print(num_list[x], end=" ")
+    if x == (len(num_list)-1):
+        print(num_list[x])
