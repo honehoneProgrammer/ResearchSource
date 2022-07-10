5c09a008041962009e320ec3a8d40e6b13016727
honehoneProgrammer <GithubIbuki.com>
2022-07-10 22:11:45 +0900

    綺麗じゃないけどcomplete

--- 4.py
+++ 4.py
@@ -1,8 +1,12 @@
 num = int(input())
 H, M, S = (60*60), 60, 1
-h, m, s = num // H, num // M, num // S
+h = num // H
 num -= h * H
+
+m = num // M
 num -= m * M
+
+s = num // S
 num -= s * S
 
 print(h, m, s, sep=":")
