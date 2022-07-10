fe61935d5e07625af02cbd0b57bf2bcb42662a05
honehoneProgrammer <GithubIbuki.com>
2022-07-11 00:12:57 +0900

    complete 最後の数字のスペースが要らなかった

--- ITP1_2_C_Sorting Three_Numbers.py
+++ ITP1_2_C_Sorting Three_Numbers.py
@@ -3,6 +3,7 @@ num_list = [int(x) for x in input().split()]
 num_list.sort()
 
 for x in range(len(num_list)):
-    print(num_list[x], end=" ")
     if x == (len(num_list)-1):
         print(num_list[x])
+        break
+    print(num_list[x], end=" ")
