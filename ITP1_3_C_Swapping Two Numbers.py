ba5cc393000dfe17746e7195d2aa2d3afe8b46b7
honehoneProgrammer <GithubIbuki.com>
2022-07-18 06:30:59 +0900

    type error

--- ITP1_3_C_Swapping Two Numbers.py
+++ ITP1_3_C_Swapping Two Numbers.py
@@ -7,6 +7,11 @@ while True:
 
 del num_list[-1]
 
+for i in range(len(num_list)):
+    num_list[i][0] = int(num_list[i][0])
+    num_list[i][1] = int(num_list[i][1])
+
+
 for num in range(num_list):
     print(num.sort())
 
