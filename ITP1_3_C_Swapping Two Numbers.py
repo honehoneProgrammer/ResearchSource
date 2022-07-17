09fa5821c5fc80182936eec693b5ec4ad9a7db15
honehoneProgrammer <GithubIbuki.com>
2022-07-18 06:26:32 +0900

    listの中身がstringだったのが原因だった

--- ITP1_3_C_Swapping Two Numbers.py
+++ ITP1_3_C_Swapping Two Numbers.py
@@ -2,13 +2,12 @@ num_list = []
 
 while True:
     num_list.append(input().split())
-    print(num_list[-1][0])
-    if (num_list[-1][0] == 0) and (num_list[-1][1] == 0):
+    if (num_list[-1][0] == "0") and (num_list[-1][1] == "0"):
         break
 
 del num_list[-1]
 
 for num in range(num_list):
-    print(num)
+    print(num.sort())
 
 # https://techacademy.jp/magazine/49549
