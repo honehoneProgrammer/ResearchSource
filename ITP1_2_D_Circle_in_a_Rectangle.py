a8c991517063a4806c88de9e78f4ab1d7ce54319
honehoneProgrammer <GithubIbuki.com>
2022-07-18 05:30:17 +0900

    complete

--- ITP1_2_D_Circle_in_a_Rectangle.py
+++ ITP1_2_D_Circle_in_a_Rectangle.py
@@ -4,6 +4,9 @@ num_list = [int(x) for x in input().split()]
 if (0 <= num_list[2]-num_list[4]) and (num_list[2]+num_list[4] <= num_list[0]):
     if (0 <= num_list[3]-num_list[4]) and (num_list[3]+num_list[4] <= num_list[1]):
         print("Yes")
-        break
+        exit()
 
 print("No")
+
+
+# https://techacademy.jp/magazine/15831 break文のとき
