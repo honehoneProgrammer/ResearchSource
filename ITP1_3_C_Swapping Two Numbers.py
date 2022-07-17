79a57957d9b0f8fa8c7ca091d015479d99f03731
honehoneProgrammer <GithubIbuki.com>
2022-07-18 06:16:26 +0900

    if文が働かない

--- ITP1_3_C_Swapping Two Numbers.py
+++ ITP1_3_C_Swapping Two Numbers.py
@@ -2,10 +2,13 @@ num_list = []
 
 while True:
     num_list.append(input().split())
-    if num_list[-1][0] == 0 and num_list[-1][1] == 0:
+    print(num_list[-1][0])
+    if (num_list[-1][0] == 0) and (num_list[-1][1] == 0):
         break
 
 del num_list[-1]
 
 for num in range(num_list):
     print(num)
+
+# https://techacademy.jp/magazine/49549
