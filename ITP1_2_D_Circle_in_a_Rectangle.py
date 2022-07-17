73bfed2f069b2781560b0bf87bbd3e5cdfe08665
honehoneProgrammer <GithubIbuki.com>
2022-07-18 05:26:46 +0900

    return syntaxerror

---
+++ ITP1_2_D_Circle_in_a_Rectangle.py
@@ -0,0 +1,9 @@
+num_list = [int(x) for x in input().split()]
+
+
+if (0 <= num_list[2]-num_list[4]) and (num_list[2]+num_list[4] <= num_list[0]):
+    if (0 <= num_list[3]-num_list[4]) and (num_list[3]+num_list[4] <= num_list[1]):
+        print("Yes")
+        return
+
+print("No")
