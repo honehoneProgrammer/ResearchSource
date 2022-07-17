e9d40f72257be1fb44b6735469fd6c6872f3e966
honehoneProgrammer <GithubIbuki.com>
2022-07-18 06:09:49 +0900

    num_list宣言し忘れ

---
+++ ITP1_3_C_Swapping Two Numbers.py
@@ -0,0 +1,9 @@
+while True:
+    num_list.append(int(input().split()))
+    if num_list[-1][0] == 0 and num_list[-1][1] == 0:
+        break
+
+del num_list[-1]
+
+for num in range(num_list):
+    print(num)
