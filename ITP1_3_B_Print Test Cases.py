82f3a42eacf3a745d36925b33a3bdd3beff45fba
honehoneProgrammer <GithubIbuki.com>
2022-07-18 06:01:07 +0900

    complete sepで対処

--- ITP1_3_B_Print Test Cases.py
+++ ITP1_3_B_Print Test Cases.py
@@ -7,6 +7,7 @@ while True:
 del num_list[-1]
 
 for i in range(len(num_list)):
-    print("Case", i+1, ":", num_list[i])
+    print("Case ", i+1, ": ", num_list[i], sep="")
 
 # https://www.modis.co.jp/candidate/insight/column_98 listの使い方
+# https://qiita.com/kenta1984/items/9423959e292a1e173bbc
