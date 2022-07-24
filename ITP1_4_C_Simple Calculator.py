227aaedd24ae265a5044984070b257de76d21736
honehoneProgrammer <GithubIbuki.com>
2022-07-24 22:12:22 +0900

    error 一気に出力

--- ITP1_4_C_Simple Calculator.py
+++ ITP1_4_C_Simple Calculator.py
@@ -7,18 +7,17 @@ while True:
     num_list[0] = int(num_list[0])
     num_list[1] = int(num_list[1])
 
-    match operand:
-        case "+":
-            print(num_list[0]+num_list[2])  
+    if operand == "+":
+        print(num_list[0]+num_list[2])
 
-        case "-":
-            print(num_list[0]+num_list[2])
+    elif operand == "-":
+        print(num_list[0]+num_list[2])
+
+    elif operand == "*":
+        print(num_list[0]+num_list[2])
+
+    elif operand == "/":
+        print(num_list[0]+num_list[2])
         
-        case "*":
-            print(num_list[0]+num_list[2])
-
-        case "/":
-            print(num_list[0]+num_list[2])
-
-        case "?":
-            break
+    elif operand == "?":
+        break
