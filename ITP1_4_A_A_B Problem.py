4731800d949f63aee948bb3866daccf3fca0ad83
honehoneProgrammer <GithubIbuki.com>
2022-07-24 21:37:16 +0900

    format指定ミス,小数点以下の桁数を表示するやつだった

--- ITP1_4_A_A_B Problem.py
+++ ITP1_4_A_A_B Problem.py
@@ -1,5 +1,5 @@
 num_list = [int(x) for x in input().split()]
 
-print(num_list[0]//num_list[1], num_list[0]%num_list[1], '{:.6g}'.format(num_list[0]/num_list[1]))
+print(num_list[0]//num_list[1], num_list[0]%num_list[1], '{:.5e}'.format(num_list[0]/num_list[1]))
 
 # https://www.headboost.jp/python-print-handle-number-of-digits/
