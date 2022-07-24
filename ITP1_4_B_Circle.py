c971c64d7058e68c2161a439edb8b603e326a101
honehoneProgrammer <GithubIbuki.com>
2022-07-24 21:47:32 +0900

    error numがint以外の方も許すらしい、piは大文字のPIじゃなかった

--- ITP1_4_B_Circle.py
+++ ITP1_4_B_Circle.py
@@ -2,7 +2,7 @@ import math
 
 num = int(input())
 
-print(f'{num*num*math.PI:.6f}', f'{2*num*math.PI:.6f}')
+print(f'{num*num*math.pi:.6f}', f'{2*num*math.pi:.6f}')
 
 # https://www.headboost.jp/python-print-handle-number-of-digits/
 # https://techacademy.jp/magazine/20606
