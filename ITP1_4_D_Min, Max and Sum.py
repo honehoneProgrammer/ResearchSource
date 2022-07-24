ee60d817b2791f5781d0937782b91e59ebd92419
honehoneProgrammer <GithubIbuki.com>
2022-07-24 22:48:15 +0900

    complete 関数使わないほうがいいかも？

---
+++ ITP1_4_D_Min, Max and Sum.py
@@ -0,0 +1,7 @@
+length = int(input())
+num_list = [int(x) for x in input().split()]
+
+print(min(num_list), max(num_list), sum(num_list))
+
+# https://note.nkmk.me/python-max-min-heapq-nlargest-nsmallest/
+# https://techacademy.jp/magazine/23326
