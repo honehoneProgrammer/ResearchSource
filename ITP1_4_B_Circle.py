481dd397fde0ffe37b0f8811ed2739803fd222a8
honehoneProgrammer <GithubIbuki.com>
2022-07-24 21:45:23 +0900

    attribute error mathにPIがない

---
+++ ITP1_4_B_Circle.py
@@ -0,0 +1,8 @@
+import math
+
+num = int(input())
+
+print(f'{num*num*math.PI:.6f}', f'{2*num*math.PI:.6f}')
+
+# https://www.headboost.jp/python-print-handle-number-of-digits/
+# https://techacademy.jp/magazine/20606
