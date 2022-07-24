0a47c08185d1061bb915cd81980873bc600308a6
honehoneProgrammer <GithubIbuki.com>
2022-07-24 21:27:34 +0900

    complete rangeの範囲ミス

--- ITP1_3_D_How Many Divisors.py
+++ ITP1_3_D_How Many Divisors.py
@@ -2,8 +2,10 @@ num_list = [int(x) for x in input().split()]
 
 divisors = 0
 
-for i in range(num_list[0], num_list[1]):
+for i in range(num_list[0], num_list[1]+1):
     if num_list[2] % i == 0:
         divisors += 1
 
 print(divisors)
+
+# https://www.javadrive.jp/python/function/index6.html
