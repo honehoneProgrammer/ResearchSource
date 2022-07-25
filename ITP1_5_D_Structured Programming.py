f318aa8ea3bf2c3668c38d67725f6bd92dbefb58
honehoneProgrammer <GithubIbuki.com>
2022-07-25 23:20:31 +0900

    complete　問題文から条件分がうまく読み取れてなかった

--- ITP1_5_D_Structured Programming.py
+++ ITP1_5_D_Structured Programming.py
@@ -5,6 +5,8 @@ for i in range(1, num+1):
     tmp = i
     if i % 3 == 0:
             print(tmp, end=" ")
+            continue
+    
     while i:
         if i % 10 == 3:
             print(tmp, end=" ")
