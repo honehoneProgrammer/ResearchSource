38f0ce27f74997b85efd62b48969eb51757f223c
honehoneProgrammer <GithubIbuki.com>
2022-07-25 14:30:27 +0900

    やっぱり途中変数が必要

--- ITP1_5_D_Structured Programming.py
+++ ITP1_5_D_Structured Programming.py
@@ -1,16 +1,7 @@
 num = int(input())
 
 for i in range(1, num+1):
-    if i % 3 == 0:
+    if i % 3 == 0 or i % 10 == 3 or i // 10 == 3:
         print(i, end=" ")
-        continue
-    
-    while True:
-        if i % 10 == 3:
-            print(i, end=" ")
-            break
-        i //= 10
-        if i == 0:
-            break
 
 # https://www.javadrive.jp/python/function/index6.html
