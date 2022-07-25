d91fe480e6357bfc9fbffe5931667e318934bfcd
honehoneProgrammer <GithubIbuki.com>
2022-07-25 14:21:11 +0900

    whileの中に命令が入っていなかった

---
+++ ITP1_5_D_Structured Programming.py
@@ -0,0 +1,14 @@
+num = int(input())
+
+for i in range(num):
+    if i % 3 == 0:
+        print(i, end=" ")
+        continue
+    
+    while True:
+    if i % 10 == 3:
+        print(i, end=" ")
+        break
+    i /= 10
+    if i == 0:
+        break
