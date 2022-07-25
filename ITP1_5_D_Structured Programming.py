d575a61c33dece86b36d2dafb67be96f817d6ba4
honehoneProgrammer <GithubIbuki.com>
2022-07-25 14:35:22 +0900

    もう一つ繰り返しが必要

--- ITP1_5_D_Structured Programming.py
+++ ITP1_5_D_Structured Programming.py
@@ -1,7 +1,10 @@
 num = int(input())
+tmp = 0
 
 for i in range(1, num+1):
-    if i % 3 == 0 or i % 10 == 3 or i // 10 == 3:
-        print(i, end=" ")
+    tmp = i
+    if i % 3 == 0 or i % 10 == 3:
+        print(tmp, end=" ")
+    i //= 10
 
 # https://www.javadrive.jp/python/function/index6.html
