5b09994b4153a7e01170e3b5425f30b3e75b2a7c
honehoneProgrammer <GithubIbuki.com>
2022-07-25 23:19:31 +0900

    continueなかった

--- ITP1_5_D_Structured Programming.py
+++ ITP1_5_D_Structured Programming.py
@@ -3,9 +3,12 @@ tmp = 0
 
 for i in range(1, num+1):
     tmp = i
-    while i:
-        if i % 3 == 0 or i % 10 == 3:
+    if i % 3 == 0:
             print(tmp, end=" ")
+    while i:
+        if i % 10 == 3:
+            print(tmp, end=" ")
+            break
         i //= 10
 
 # https://www.javadrive.jp/python/function/index6.html
