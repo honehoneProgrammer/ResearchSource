298459079227b7a6f5aaaabdc407232c4f156123
honehoneProgrammer <GithubIbuki.com>
2022-07-25 14:37:19 +0900

    while文の中break必要

--- ITP1_5_D_Structured Programming.py
+++ ITP1_5_D_Structured Programming.py
@@ -3,8 +3,9 @@ tmp = 0
 
 for i in range(1, num+1):
     tmp = i
-    if i % 3 == 0 or i % 10 == 3:
-        print(tmp, end=" ")
-    i //= 10
+    while i:
+        if i % 3 == 0 or i % 10 == 3:
+            print(tmp, end=" ")
+        i //= 10
 
 # https://www.javadrive.jp/python/function/index6.html
