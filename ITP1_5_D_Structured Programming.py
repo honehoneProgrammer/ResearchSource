86e978d7bfc7f1ac1d53d4aa43166cda093d25d7
honehoneProgrammer <GithubIbuki.com>
2022-08-05 07:00:20 +0900

    first

--- ITP1_5_D_Structured Programming.py
+++ ITP1_5_D_Structured Programming.py
@@ -1,16 +1,14 @@
 num = int(input())
-tmp = 0
 
 for i in range(1, num+1):
-    tmp = i
     if i % 3 == 0:
-            print(tmp, end=" ")
-            continue
+        print(i, end=" ")
+        continue
     
-    while i:
+    while True:
         if i % 10 == 3:
-            print(tmp, end=" ")
+            print(i, end=" ")
             break
         i //= 10
-
-# https://www.javadrive.jp/python/function/index6.html
+        if i == 0:
+            break
