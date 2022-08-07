a3e11395c917c1d8f350012f1241c3bc5c178bbd
honehoneProgrammer <GithubIbuki.com>
2022-08-08 07:32:56 +0900

    first

--- ITP1_6_A_Reversing Numbers.py
+++ ITP1_6_A_Reversing Numbers.py
@@ -1,10 +1,13 @@
 length = int(input())
-num_list = [int(x) for x in input().split()]
+num_list = [x for x in input().split()]
 
 num_list.reverse()
-
+element = ""
 for ele in num_list:
-    if ele == num_list[-1]:
-        print(ele, end="")
+    element = ele
+    if element == num_list[-1]:
+        print(element, end="")
         break
     print(ele, end=" ")
+
+# print(" ".join(num_list))
