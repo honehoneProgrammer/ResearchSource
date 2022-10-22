1eaa04196c07246b0acc91be95371409854c69ea
honehoneProgrammer <GithubIbuki.com>
2022-10-23 07:05:46 +0900

    31

--- Sorting num 2.py
+++ Sorting num 2.py
@@ -5,6 +5,7 @@ for i in inp.split():
     num_list.append(int(i))
 
 min = 100000
+minindex = -1
 ans = []
 l = len(num_list)
 
@@ -12,10 +13,10 @@ for j in range(l):
     min = 100000
     l2 = len(num_list)
     for i in range(l2):
-        print(i)
-        print(num_list)
         if min > num_list[i]:
-            min = num_list.pop(i)
+            min = num_list[i]
+            minindex = i
+    num_list.pop(minindex)
     ans.append(min)
 
 print(ans[0], ans[1], ans[2])
