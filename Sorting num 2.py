894e77c8124eb7d1e16a7a5978b653e8042ee09b
honehoneProgrammer <GithubIbuki.com>
2022-10-23 06:28:18 +0900

    15

--- Sorting num 2.py
+++ Sorting num 2.py
@@ -4,20 +4,20 @@ inp = input()
 for i in inp.split():
     num_list.append(int(i))
 
-if num_list[0] < num_list[1] < num_list[2]:
+if num_list[0] <= num_list[1] <= num_list[2]:
     print(num_list[0], num_list[1], num_list[2])
 
-elif num_list[2] < num_list[1] < num_list[0]:
+elif num_list[2] <= num_list[1] <= num_list[0]:
     print(num_list[2], num_list[1], num_list[0])
 
-elif num_list[0] < num_list[2] < num_list[1]:
+elif num_list[0] <= num_list[2] <= num_list[1]:
     print(num_list[0], num_list[2], num_list[1])
 
-elif num_list[1] < num_list[0] < num_list[2]:
+elif num_list[1] <= num_list[0] <= num_list[2]:
     print(num_list[1], num_list[0], num_list[2])
 
-elif num_list[1] < num_list[2] < num_list[0]:
+elif num_list[1] <= num_list[2] <= num_list[0]:
     print(num_list[1], num_list[2], num_list[0])
 
-elif num_list[2] < num_list[0] < num_list[1]:
+elif num_list[2] <= num_list[0] <= num_list[1]:
     print(num_list[2], num_list[0], num_list[1])
