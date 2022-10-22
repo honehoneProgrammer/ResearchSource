1b09d355a71970123f387ba7d35776fff3aa2f1a
honehoneProgrammer <GithubIbuki.com>
2022-10-23 06:46:07 +0900

    22

--- Sorting num 2.py
+++ Sorting num 2.py
@@ -7,7 +7,7 @@ for i in inp.split():
 min = 100000
 ans = []
 
-for j in range(0,len(num_list), -1):
+for j in range(len(num_list)-1, -1,-1):
     for i in range(j):
         if min > num_list[i]:
             min = num_list[i]
