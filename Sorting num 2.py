fc1054ff00e5c2f1120e69a8ff3cb6c16a34651a
honehoneProgrammer <GithubIbuki.com>
2022-10-23 06:36:57 +0900

    17

--- Sorting num 2.py
+++ Sorting num 2.py
@@ -4,11 +4,7 @@ inp = input()
 for i in inp.split():
     num_list.append(int(i))
 
-min = 100000
-ans = []
+num_list.sort()
 
-for j in range(len(num_list)):
-    for i in range(j):
-        if min > num_list[i]:
-            min = num_list[i]
+print(num_list[0], num_list[1], num_list[2])
 
