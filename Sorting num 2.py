c1649c6ca324d9eaac6f89f146cd5eff1250f8bb
honehoneProgrammer <GithubIbuki.com>
2022-10-23 06:22:55 +0900

    11

--- Sorting num 2.py
+++ Sorting num 2.py
@@ -4,5 +4,5 @@ inp = input()
 for i in inp.split():
     num_list.append(int(i))
 
-if num_list[1] < num_list[2] < num_list[3]:
+if num_list[0] < num_list[1] < num_list[2]:
     print(num_list[1], num_list[2], num_list[3])
