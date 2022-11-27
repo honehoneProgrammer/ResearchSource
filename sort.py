e2c12eab3fc4dd601acd3c026c14e6642c0d77c7
honehoneProgrammer <GithubIbuki.com>
2022-11-28 08:26:07 +0900

    16

--- sort.py
+++ sort.py
@@ -4,11 +4,3 @@ for i in range(1, num+1):
     if i % 3 == 0:
         print(i, end=" ")
         continue
-    
-    while True:
-        if i % 10 == 3:
-            print(i, end=" ")
-            break
-        i //= 10
-        if i == 0:
-            break
