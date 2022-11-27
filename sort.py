90dd588934c9642721e1c0343a3c7560c1b8a6f4
honehoneProgrammer <GithubIbuki.com>
2022-11-28 07:33:07 +0900

    7

--- sort.py
+++ sort.py
@@ -2,3 +2,14 @@ import math
 
 num_list = []
 operand = ""
+
+for num in num_list:
+    operand = num[1]
+    if operand == "+":
+        print(num[0]+num[2])
+
+    elif operand == "-":
+        print(num[0]-num[2])
+
+    elif operand == "*":
+        print(num[0]*num[2])
