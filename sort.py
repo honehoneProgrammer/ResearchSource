2929487ea58a150b1935b6c08db1f2cb6854fbc7
honehoneProgrammer <GithubIbuki.com>
2022-11-28 07:30:23 +0900

    4

--- sort.py
+++ sort.py
@@ -6,4 +6,9 @@ if a <= c and c <= b:
     print(a, c, b)
 if b <= a and a <= c:
     print(b, a, c)
-
+if b <= c and c <= a:
+    print(b, c, a)
+if c <= a and a <= b:
+    print(c, a, b)
+if c <= b and b <= a:
+    print(c, b, a)
