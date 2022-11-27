68cb1c7f80cf6c37eefd96c1d957143d0aeaa51a
honehoneProgrammer <GithubIbuki.com>
2022-11-28 08:26:31 +0900

    17

--- sort.py
+++ sort.py
@@ -1,6 +1,18 @@
-num = int(input())
+num_list = []
 
-for i in range(1, num+1):
-    if i % 3 == 0:
-        print(i, end=" ")
-        continue
+
+while True:
+    num_list.append(input().split())
+    if (num_list[-1][0] == "0") and (num_list[-1][1] == "0"):
+        break
+
+del num_list[-1]
+
+for i in range(len(num_list)):
+    num_list[i][0] = int(num_list[i][0])
+    num_list[i][1] = int(num_list[i][1])
+
+
+for num in num_list:
+    num.sort()
+    print(num[0], num[1])
