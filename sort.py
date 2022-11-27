dffed49884fccfcdea52a1af51663350d43d5ced
honehoneProgrammer <GithubIbuki.com>
2022-11-28 07:30:55 +0900

    5

--- sort.py
+++ sort.py
@@ -1,14 +1,3 @@
-a,b,c=input().split()
-
-if a <= b and b <= c:
-    print(a, b, c)
-if a <= c and c <= b:
-    print(a, c, b)
-if b <= a and a <= c:
-    print(b, a, c)
-if b <= c and c <= a:
-    print(b, c, a)
-if c <= a and a <= b:
-    print(c, a, b)
-if c <= b and b <= a:
-    print(c, b, a)
+ans = [[0 for i in range(10)] for j in range(12)]
+length = int(input())
+num_list = list()
