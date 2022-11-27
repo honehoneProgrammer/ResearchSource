3b7f93c1e9ed637e0c50240b5447bfdfd74379be
honehoneProgrammer <GithubIbuki.com>
2022-11-28 08:25:14 +0900

    13

--- sort.py
+++ sort.py
@@ -1,2 +1,27 @@
-for i in range(1000):
-    print("Hello World")
+num_list = []
+
+while True:
+    num_list.append(input().split())
+
+    num_list[-1][0] = int(num_list[-1][0])
+    num_list[-1][1] = int(num_list[-1][1])
+
+    if num_list[-1][0] == 0 and num_list[-1][1] == 0:
+        break
+
+for num in num_list:
+    for i in range(num[0]):
+        for j in range(num[1]):
+            if i % 2 == 0:
+                if j % 2 == 0:
+                    print("#", end="")
+                else:
+                    print(".", end="")
+
+            else:
+                if j % 2 == 0:
+                    print(".", end="")
+                else:
+                    print("#", end="")
+        print()
+    print()
