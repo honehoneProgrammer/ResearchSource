53b6bd33279bb2e6364eeb3c0434503f78969508
honehoneProgrammer <GithubIbuki.com>
2022-11-28 08:26:43 +0900

    18

--- sort.py
+++ sort.py
@@ -11,8 +11,3 @@ del num_list[-1]
 for i in range(len(num_list)):
     num_list[i][0] = int(num_list[i][0])
     num_list[i][1] = int(num_list[i][1])
-
-
-for num in num_list:
-    num.sort()
-    print(num[0], num[1])
