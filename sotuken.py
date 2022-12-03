ebebb17facd769ab77d22a9a950e874e6dba9a47
honehoneProgrammer <GithubIbuki.com>
2022-12-03 20:42:20 +0900

    low nakayama

--- sotuken.py
+++ sotuken.py
@@ -1,3 +1,20 @@
-ary = list(map(int, input().split()))
-res = sorted(ary)
-print('{} {} {}'.format(res[0],res[1],res[2]))
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+if num1>num2:
+    tmp = num2
+    num2 = num1
+    num1 = tmp
+
+if num2>num3:
+    tmp = num3
+    num3 = num2
+    num2 = tmp
+
+    if num1>num2:
+        tmp = num2
+        num2 = num1
+        num1 = tmp
+
+print('%d %d %d' %(num1, num2, num3))
