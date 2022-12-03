b49e990fb70632c7b67577f598ce729e99ecc31a
honehoneProgrammer <GithubIbuki.com>
2022-12-03 20:45:58 +0900

    4

--- sotu.py
+++ sotu.py
@@ -1,25 +1,12 @@
-a, b, c= map(int, input().split())
-if a > b:
-    if a > c:
-        max = a;
-        if b > c:
-            mid = b
-            min = c
-        else:
-            mid = c
-            min = b
-    else:
-        max = c
-        mid = a
-        min = b
-elif a > c:
-    max = b
-    mid = a
-    min = c
-else:
-    min = a
-    max = b
-    mid = c
+input_string = "3 2 1"
 
+nums = input_string.split(" ")
 
-print('{} {} {}'.format(min,mid,max))
+for _ in range(len(nums)-1):
+	for i in range(len(nums)-1):
+		if(nums[i] > nums[i+1]):
+			tmp = nums[i+1]
+			nums[i] = nums[i+1]
+			nums[i+1] = tmp
+
+print(" ".join(nums))
