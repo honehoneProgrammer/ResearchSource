ba1fd4fbdb3c01284e02f8c95a3698f86a298c14
honehoneProgrammer <GithubIbuki.com>
2022-12-18 15:20:23 +0900

    17

--- official.py
+++ official.py
@@ -1,12 +1,2 @@
-import numpy as np
-
-ans = np.zeros((12, 10))
-
-length = int(input())
-num_list = list()
-ans = []
-
-for i in range(length):
-    tmp_list = [x for x in input().split()]
-    ans[(tmp_list[0]-1)*3+(tmp_list[1]-1)][tmp_list[2]-1] += tmp_list[3]
-
+for i in range(1000):
+    print("Hello World")
