548195f515adb3a9e8ae9d4e9b3c110e7bec2778
honehoneProgrammer <GithubIbuki.com>
2022-12-18 15:02:04 +0900

    13

--- official.py
+++ official.py
@@ -2,7 +2,12 @@ ans = [[0 for i in range(10)] for j in range(12)]
 length = int(input())
 num_list = list()
 
-for i in range(length):
-    tmp_list = [int(x) for x in input().split()]
-    ans[(tmp_list[0]-1)*3+(tmp_list[1]-1)][tmp_list[2]-1] += tmp_list[3]
-
+for i in range(12):
+    print(" ", end='')
+    for j in range(10):
+        if j == 9:
+            print('%d' % ans[i][j])
+            break
+        print('%d' % ans[i][j], end=" ")
+    if i % 3 == 2 and not i == 11:
+        print("####################")
