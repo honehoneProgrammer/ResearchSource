a9b71551d05c2b09c9320cb19ee4601ac1bc8709
honehoneProgrammer <GithubIbuki.com>
2022-12-18 15:19:28 +0900

    16

--- official.py
+++ official.py
@@ -5,3 +5,8 @@ ans = np.zeros((12, 10))
 length = int(input())
 num_list = list()
 ans = []
+
+for i in range(length):
+    tmp_list = [x for x in input().split()]
+    ans[(tmp_list[0]-1)*3+(tmp_list[1]-1)][tmp_list[2]-1] += tmp_list[3]
+
