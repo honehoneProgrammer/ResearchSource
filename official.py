80acf617f578ef259d598134a51b0af4a03b71ba
honehoneProgrammer <GithubIbuki.com>
2022-12-18 14:55:18 +0900

    5

--- official.py
+++ official.py
@@ -1,12 +1,1 @@
-num_list = []
-
-while True:
-    num_list.append(input().split())
-
-    num_list[-1][0] = int(num_list[-1][0])
-    num_list[-1][1] = int(num_list[-1][1])
-
-    if num_list[-1][0] == 0 and num_list[-1][1] == 0:
-        break
-    
-    print("####################")
+print("####################")
