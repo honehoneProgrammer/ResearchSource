cd3f5274303bba331fa3919ce2eeac7c53a30916
honehoneProgrammer <GithubIbuki.com>
2022-12-18 15:00:21 +0900

    10

--- official.py
+++ official.py
@@ -1,5 +1,17 @@
-import math
+num_list = []
 
-num = float(input())
+while True:
+    num_list.append(input().split())
 
-print(f'{num*num*math.pi:.6f}', f'{2*num*math.pi:.6f}')
+    num_list[-1][0] = int(num_list[-1][0])
+    num_list[-1][1] = int(num_list[-1][1])
+
+    if num_list[-1][0] == 0 and num_list[-1][1] == 0:
+        break
+
+for num in num_list:
+    for i in range(num[0]):
+        for j in range(num[1]):
+            print("#", end="")
+        print()
+    print()
