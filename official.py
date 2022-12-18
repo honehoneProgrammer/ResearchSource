503875fb98125f61594d88b9ecaefbf677f32b21
honehoneProgrammer <GithubIbuki.com>
2022-12-18 15:20:39 +0900

    18

--- official.py
+++ official.py
@@ -1,2 +1,33 @@
-for i in range(1000):
-    print("Hello World")
+import math
+
+num_list = []
+operand = ""
+
+while True:
+    num_list.append(input().split())
+    operand = num_list[-1][1]
+    if operand == "?":
+        break
+
+for i in range(len(num_list)):
+    num_list[i][0] = int(num_list[i][0])
+    num_list[i][2] = int(num_list[i][2])
+
+for num in num_list:
+    operand = num[1]
+    if operand == "+":
+        print(num[0]+num[2])
+
+    elif operand == "-":
+        print(num[0]-num[2])
+
+    elif operand == "*":
+        print(num[0]*num[2])
+
+    elif operand == "/":
+        print(math.floor(num[0]/num[2]))
+
+    elif operand == "?":
+        break
+
+# https://note.nkmk.me/python-math-floor-ceil-int/
